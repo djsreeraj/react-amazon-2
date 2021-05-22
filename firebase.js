93a2a40e92bff4725700b7236b160a90658bd007
djsreeraj <sreeraj011m@gmail.com>
2021-05-22 18:44:33 +0530

    added auth, updated checkoutpage

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDSXvtRtsDAaD42DKhAbDE484q2qTDrE38",
+  authDomain: "amzn-2-65024.firebaseapp.com",
+  projectId: "amzn-2-65024",
+  storageBucket: "amzn-2-65024.appspot.com",
+  messagingSenderId: "383250064399",
+  appId: "1:383250064399:web:5a98858d8fd5ef0dde2f7a",
+};
